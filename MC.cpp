fb9a5a4e3e6c15201734b9360bac631e3f57ac67
Aieener <aikurt@hotmail.com>
2016-01-18 17:14:12 -0500

    still not finished but in progress

--- MC.cpp
+++ MC.cpp
@@ -222,252 +222,6 @@ void MC::Del(Cells &s,double &prob,double &probd, double &size)
 	}	
 }
 
-// void MC::Add(Cells &s,array<double,100000> &WF,double &prob,double &size,double &S,double &deltaS)
-// {
-// 	double aaccp,proba;      // the acceptance probabilities: 
-// 	double V = double(n0*n1*n2);    // the total lattice size
-// 	double K = double(length); //
-// 	int x,y,z,o; // pick a random position and orientation for the HR to be added;
-// 	x = rand()%n0; // x range[0,n0-1]
-// 	y = rand()%n1; // y range[0,n1-1]
-// 	z = rand()%n2; // z range[0,n2-1]
-// 	o = rand()%3  ; // 0 range {0,1,2}
-//     int j_0 = S/(deltaS/2); // the initial state index
-//     // cout << S<<"  "<<j_0<<endl;
-
-//     int j_1; // the state after movement
-
-// 	if(s.getSquare(x,y,z).isEmpty()) // if it's open, try to do Addition;
-// 	{
-// 		HR rod(x,y,z,length,o);
-
-// 		if(o == 0)
-// 		{
-// 		//======================== Vertical ===============================
-// 			// the vertical case
-// 			int counter = 0;
-
-// 			for (int j = 0; j < length-1; j++)
-// 			{
-// 				// check if the vertical space is wide open
-// 				if(s.getSquare(x,(y+j+1)%n1,z).isOccupied())
-// 				{
-// 					counter++;
-// 					break;
-// 				}						
-// 			}
-// 			// cout <<"counter = "<< counter<< endl;
-// 			if (counter == 0)
-// 			{
-// 				av++;
-// 				nv++;// accumulate the # of ver rod;
-// 				j_1 = S/(deltaS/2);
-// 				aaccp = (z*V)/((size+1.0)*K)*(exp(WF[j_1] - WF[j_0])); // Have to revise!
-// 				proba = min(1.0,aaccp);
-
-// 				if(prob<=proba)
-// 				{
-// 					// Do addition;
-// 					// push the new rod into the VRodlist;
-// 					VRodlist.push_back(rod);
-// 					// update new N, E and new config;
-// 					for (int i = 0; i < length; i++)
-// 					{	
-// 						s.getSquare(x,(y+i)%n1,z).setStatus(1);
-// 					}					
-// 				}
-// 				else
-// 				{ // movement failed, refine av and nv
-// 					av--;
-// 					nv--;// accumulate the # of ver rod;
-// 				}		
-// 			}									
-// 		}
-
-// 		else if(o == 1)
-// 		{
-//         //======================= Horizontal  ============================
-// 			int counter = 0;
-// 			for (int j = 0; j< length-1 ; j++)
-// 			{
-// 				// check if the horizontal space is wide open
-// 				if(s.getSquare((x+1+j)%n0,y,z).isOccupied())
-// 				{
-// 					counter++;
-// 					break;
-// 				}							
-// 			}
-// 			if (counter == 0)
-// 			{
-// 				ah++;
-// 				nh++;// accumulate the # of hor rod;
-// 				j_1 = S/(deltaS/2);
-// 				aaccp = (z*V)/((size+1.0)*K)*(exp(WF[j_1] - WF[j_0])); // Have to revise!
-// 				proba = min(1.0,aaccp);
-
-// 				if(prob<= proba)
-// 				{
-// 					//Do addition;
-// 					//push the new rod into the HRodlist;
-// 					HRodlist.push_back(rod);
-// 					// update new N, E and new config;
-// 					for (int i = 0; i < length; i++)
-// 					{
-// 						s.getSquare((x+i)%n0,y,z).setStatus(1);
-// 					}
-// 				}
-// 				else
-// 				{ // movement failed, refine av and nv
-// 					ah--;
-// 					nh--;// accumulate the # of ver rod;
-// 				}	
-// 			}
-// 		}
-// 		else 
-// 		{
-//         //======================= Up  ============================
-// 			int counter = 0;
-// 			for (int j = 0; j< length-1 ; j++)
-// 			{
-// 				// check if the horizontal space is wide open
-// 				if(s.getSquare(x,y,(z+j+1)%n2).isOccupied())
-// 				{
-// 					counter++;
-// 					break;
-// 				}							
-// 			}
-// 			if (counter == 0)
-// 			{
-// 				au++;
-// 				nu++;// accumulate the # of hor rod;
-// 				j_1 = S/(deltaS/2);
-// 				aaccp = (z*V)/((size+1.0)*K)*(exp(WF[j_1] - WF[j_0])); // Have to revise!
-// 				proba = min(1.0,aaccp);
-// 				if(prob<= proba)
-// 				{
-// 					//Do addition;
-// 					//push the new rod into the HRodlist;
-// 					URodlist.push_back(rod);
-
-// 					// update new N, E and new config;
-// 					for (int i = 0; i < length; i++)
-// 					{
-// 						s.getSquare(x,y,(z+i)%n2).setStatus(1);
-// 					}
-// 				}
-// 				else
-// 				{ // movement failed, refine av and nv
-// 					au--;
-// 					nu--;// accumulate the # of ver rod;
-// 				}	
-// 			}							
-// 		}
-//     }
-// }
-
-// void MC::Del(Cells &s,array<double,100000> &WF,double &prob, double &size,double &S,double &deltaS)
-// {
-// 	double daccp,probd;      // the acceptance probabilities:
-// 	double V = double(n0*n1*n2);    // the total lattice size
-// 	double K = double(length); // 
-//     int j_0 = S/(deltaS/2); // the initial state index
-//     int j_1; // the state after movement
-
-// 	if(nv + nh + nu >0)// make sure there are Vertical rod;
-// 	{
-// 		int indx; // pick a random index from the Rodlist;
-// 		indx = rand()%int(nv+nh+nu);
-
-// 		//remove Rodlist[indx];
-// 		int x,y,z;// the position of the target on the cells;
-
-// 		if(indx < nv)
-// 		{
-// 			x = VRodlist[indx].getX();
-// 			y = VRodlist[indx].getY();
-// 			z = VRodlist[indx].getZ();	
-// 			// --------------------- it's a vertical rod -----------------------
-// 			nv--;// substract the # of ver rod;
-// 			dv++;
-// 			j_1 = S/(deltaS/2);			
-// 			daccp = (size*K)/(z*V)*(exp(WF[j_1] - WF[j_0]));	//Have to revise!
-// 			probd = min(1.0,daccp);
-
-// 			if(prob <= probd)
-// 			{
-// 				//Do deletion
-// 				for(int i = 0; i<VRodlist[indx].getLength(); i++)
-// 				{
-// 					// update the new config of cells
-// 					s.getSquare(x,(y+i)%n1,z).setStatus(0);
-// 				}
-// 				// remove the target rod from the vector Rodlist;
-// 				VRodlist.erase(VRodlist.begin() + indx);
-// 			}
-// 			else
-// 			{
-// 				// deletion failed, refine nv and dv
-// 				nv++;
-// 				dv--;
-// 			}
-// 		}
-		
-// 		else if (indx < nv + nh)
-// 		{
-// 			x = HRodlist[indx - nv].getX();
-// 			y = HRodlist[indx - nv].getY();
-// 			z = HRodlist[indx - nv].getZ();	
-// 			// --------------------- it's a horizontal rod -----------------------
-// 			nh--;// substract the # of ver rod;
-// 			dh++;
-// 			j_1 = S/(deltaS/2);			
-// 			daccp = (size*K)/(z*V)*(exp(WF[j_1] - WF[j_0]));	//Have to revise!
-// 			probd = min(1.0,daccp);
-// 			if(prob <= probd)
-// 			{
-// 				for(int i = 0; i<HRodlist[indx-nv].getLength(); i++)
-// 				{
-// 					// update the new config of cells
-// 					s.getSquare((x+i)%n0,y,z).setStatus(0);
-// 				}
-// 				// remove the target rod from the vector Rodlist;
-// 				HRodlist.erase(HRodlist.begin() + indx - nv);				
-// 			}
-// 			else
-// 			{
-// 				nh++;
-// 				dh--;
-// 			}
-// 		}
-// 		else
-// 		{
-// 			x = URodlist[indx - nv - nh].getX();
-// 			y = URodlist[indx - nv - nh].getY();
-// 			z = URodlist[indx - nv - nh].getZ();	
-// 			// --------------------- it's a up rod -----------------------
-// 			nu--;// substract the # of ver rod;
-// 			du++;
-// 			j_1 = S/(deltaS/2);			
-// 			daccp = (size*K)/(z*V)*(exp(WF[j_1] - WF[j_0]));	//Have to revise!
-// 			probd = min(1.0,daccp);
-// 			if(prob <= probd)
-// 			{
-// 				for(int i = 0; i<URodlist[indx-nv-nh].getLength(); i++)
-// 				{
-// 					// update the new config of cells
-// 					s.getSquare(x,y,(z+i)%n2).setStatus(0);
-// 				}
-// 				// remove the target rod from the vector Rodlist;
-// 				URodlist.erase(URodlist.begin()+indx-nv-nh);				
-// 			}
-// 			else
-// 			{
-// 				nu++;
-// 				du--;
-// 			}
-// 		}
-// 	}	
-// }
 /*
 ***************************** The idea of weight on S **************************
 * Dividing the order parameter axis into equally sized intervals of width DeltaS
@@ -512,13 +266,7 @@ array<double,100000> MC::MCSUS()
 	array<double,100000> PH_w;
 	array<double,100000> PL_w;
 
-	//================================Start my SUS_MC simulation=================================
-    // Weights WF = [(s=0.0005),(s=0.001),(s=0.0015),(s=0.002) ... , (s=0.5)] ---> say 0.5/0.0005 = 1000 components
-    // Windows = [{(s=0.0005),(s=0.001)}; {(s=0.001),(s=0.0015)};...;{(s=0.4995),(s=0.5)}] ---> 0.4995/0.0005 = 999 windows
-    // !! the while loop start from a state that when S is in [0,deltaS] !!
-	//===========================================================================================
-
-    // ============= Do GCMC to reach the initial state where S is in [0,deltaS] ==========
+    // ============= Do a GCMC to reach the initial state where S is in [0,deltaS] ==========
 	while (S <= 0 || S>DeltaS)
 	{
 		S = (nu-(nv+nh)*0.5)/(nu+nv+nh);
@@ -530,7 +278,6 @@ array<double,100000> MC::MCSUS()
 		tho = double(length*size)/double(n0*n1*n2);
 
 		aaccp = z*double(n0*n1*n2)/(double(nh+nv+nu+1.0)*double(length));
-
 		daccp = (double(nh+nv+nu)*double(length))/(z*double(n0*n1*n2));
 
 		proba = min(1.0,aaccp);
@@ -551,6 +298,11 @@ array<double,100000> MC::MCSUS()
 		cout << S << endl;			
 	}
 	// cout << "LOL" << endl;
+	//================================Start my SUS_MC simulation=================================
+    // Weights WF = [(s=0.0005),(s=0.001),(s=0.0015),(s=0.002) ... , (s=0.5)] ---> say 0.5/0.0005 = 1000 components
+    // Windows = [{(s=0.0005),(s=0.001)}; {(s=0.001),(s=0.0015)};...;{(s=0.4995),(s=0.5)}] ---> 0.4995/0.0005 = 999 windows
+    // !! the while loop start from a state that when S is in [0,deltaS] !!
+	//===========================================================================================
 
 	// ================================== THE SUS on S simulation starts ==================================
 	while (w <= N_window)// the while loop terminates when finish simulating the last window: window[V/K]; !!!for rods: window[0.8*V/K]
@@ -566,29 +318,21 @@ array<double,100000> MC::MCSUS()
 			addordel = rand()%2;
 		    double size = nv+nh+nu;
 		    double N = int(size);
-		    double S = 0;
 		    if(nu+nv+nh != 0)
 		    {
-		    	S = (nu-(nv+nh)*0.5)/(nu+(nv+nh)*0.5);
+				S = (nu-(nv+nh)*0.5)/(nu+nv+nh);
 		    } 
 			prob = ((double) rand() / (RAND_MAX)); 
 
 	        // ===========================Addition ===================================
 			if(addordel == 0) 
 			{
-				//For each window, N is freely fluctuated		
-				//try to Do Addition;
-				// Add(s,WF,prob,size,S,DeltaS);
 				Add(s,prob,proba);
-
 			}
 			// ============================Deletion=============================
 			else 
 			{
-				//For each window, N is freely fluctuated
-				//Do deletion;
 				Del(s,prob,probd,size);	
-
 			}	
 
 			if ((w/2.0)*DeltaS <= S && S < (w+1.0)*DeltaS/2.0) // update the fu after each step.
